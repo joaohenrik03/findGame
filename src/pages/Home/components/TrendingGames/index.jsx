69af92a9f0dbf7744b86e5a3e3e03f0c1acca231
joaohenrik03 <jh.signori@gmail.com>
2024-03-23 18:18:21 -0300

    :sparkles: search for top-rated games in the api and list them on the home page

--- src/pages/Home/components/TrendingGames/index.jsx
+++ src/pages/Home/components/TrendingGames/index.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { api, apiKey } from "../../../../services/api";
 import { Card } from "../../../../components/Card";
 import { 
   TrendingGamesContainer, 
@@ -6,39 +8,22 @@ import {
 } from "./styles";
 
 export function TrendingGames() {
-  const tempCategorys = [{
-    id: 4,
-    name: "testeasdasdasdfddfgdf",
-    slug: "action",
-    games_count: 178956,
-    image_background: "https://media.rawg.io/media/games/d82/d82990b9c67ba0d2d09d4e6fa88885a7.jpg",
-    games: []
-  },
-  {
-    id: 5,
-    name: "teste212asdasd123dfsdfdsdf",
-    slug: "action",
-    games_count: 178956,
-    image_background: "https://media.rawg.io/media/games/d82/d82990b9c67ba0d2d09d4e6fa88885a7.jpg",
-    games: []
-  },
-  {
-    id: 6,
-    name: "teste3asdasd12312asdfsdf",
-    slug: "action",
-    games_count: 178956,
-    image_background: "https://media.rawg.io/media/games/d82/d82990b9c67ba0d2d09d4e6fa88885a7.jpg",
-    games: []
-  },
-  {
-    id: 7,
-    name: "teste4asdasdasd123asdasd",
-    slug: "action",
-    games_count: 178956,
-    image_background: "https://media.rawg.io/media/games/d82/d82990b9c67ba0d2d09d4e6fa88885a7.jpg",
-    games: []
-  },
-  ];
+  const [topRatedGames, setTopRatedGames] = useState([]);
+
+  useEffect(() => {
+    async function loadTrendingGames() {
+      const response = await api.get(`/games?&key=${apiKey}`, {
+        params: {
+          "ordering": "-rating",
+          "page_size": 5,
+        }
+      })
+
+      setTopRatedGames(response.data.results);
+    }
+
+    loadTrendingGames();
+  }, [setTopRatedGames])
 
   return (
     <TrendingGamesContainer>
@@ -48,9 +33,9 @@ export function TrendingGames() {
       </TrendingGamesTitle>
 
       <TrendingGamesList
-        data={tempCategorys}
+        data={topRatedGames}
         keyExtractor={(item) => item.id}
-        renderItem={() => <Card />}
+        renderItem={({item}) => <Card data={item} />}
       />
     </TrendingGamesContainer>
   )
