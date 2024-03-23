fec1fc34ef37e7ca96a5901dbeab8fda0691c7cf
joaohenrik03 <jh.signori@gmail.com>
2024-03-23 18:28:17 -0300

    :sparkles: use dynamic information on the card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,19 +6,21 @@ import {
   Rating, 
 } from "./styles";
 
-export function Card() {
+export function Card({data}) {
   return (
     <CardContainer>
       <Image
-        source={{ uri: 'https://images.unsplash.com/photo-1626971891549-a26b62c68eb1?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
+        source={
+          false ? { uri: data.background_image } : require('../../assets/default-bg.jpg')
+        }
       />
 
       <GameDataBox>
         <Name>
-          CyberPunk 2077
+          {data.name}
         </Name>
         <Rating>
-          6.9/10
+          {data.rating.toFixed(1)}/5
         </Rating>
       </GameDataBox>
     </CardContainer>
