1035f277cbce08803c722821461031307f33eada
joaohenrik03 <jh.signori@gmail.com>
2024-03-26 21:46:44 -0300

    :sparkles: create favorites page component base

---
+++ src/pages/Favorites/index.jsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { FavoritesContainer } from "./styles";
+
+export function Favorites() {
+  return (
+    <FavoritesContainer>
+      <Text>Favorites</Text>
+    </FavoritesContainer>
+  )
+}
---
+++ src/pages/Favorites/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const FavoritesContainer = styled.SafeAreaView`
+  flex: 1;
+  background-color: #050B18;
+`;
