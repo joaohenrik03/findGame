03686a83b10399b2b17f85f742f0d559e12f0265
joaohenrik03 <jh.signori@gmail.com>
2024-03-29 22:30:45 -0300

    :sparkles: customize empty favorites list

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,11 +1,21 @@
 import { useContext } from "react";
 import { FavoritesContext } from "../../contexts/FavoritesContext";
+import Feather from 'react-native-vector-icons/Feather';
 import { Card } from "../../components/Card";
 import { Loading } from "../../components/Loading";
-import { FavoritesContainer, FavoritesList } from "./styles";
+import { EmptyList, EmptyText, FavoritesContainer, FavoritesList } from "./styles";
 
 export function Favorites() {
   const { favoriteGames, favoriteLoading } = useContext(FavoritesContext);
+
+  if (favoriteGames.length === 0) {
+    return (
+      <EmptyList>
+        <Feather name="alert-circle" size={36} color="#FFF" />
+        <EmptyText>Empty favorites list!</EmptyText>
+      </EmptyList>
+    )
+  }
 
   return (
     <FavoritesContainer>
--- src/pages/Favorites/styles.js
+++ src/pages/Favorites/styles.js
@@ -10,3 +10,14 @@ export const FavoritesList = styled.FlatList`
   flex: 1;
   padding: 0px 12px;
 `;
+
+export const EmptyList = styled(FavoritesContainer)`
+ justify-content: center;
+  align-items: center;
+`;
+
+export const EmptyText = styled.Text`
+  color: #fff;
+  font-size: 32px;
+  padding-top: 12px;
+`;
